56672b50561cdf9efbb5982edc938a9e48a45081
jw0293 <k38836919@gmail.com>
2022-07-07 22:09:51 +0900

    프로젝트 생성

---
+++ src/test/java/com/jw0293/study/springwebservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw0293.study.springwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
